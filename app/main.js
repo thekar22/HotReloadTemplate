65cfa3f497bb15c3e839cb2fe517296e5cae2276
Kyle Rego <kyle.rego@algolia.com>
2017-01-24 13:48:58 +0100

    hot reload configuration and transpiling of sass and es2015 javascript; using yarn + webpack

---
+++ app/main.js
@@ -0,0 +1,6 @@
+require('../build/index.html');
+require('./component.js');
+require('./style.sass')
+
+
+let es2015_test = [0,2,4,6,8].map(v => v + 1);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require("path");
+module.exports = {
+  entry: {
+    app: ["./app/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  }, 
+
+  module: {
+		loaders: [
+		{
+			test: /\.html$/,
+			loader: "raw-loader"
+		},
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015']
+			}
+		},
+		{
+			test: /\.sass$/,
+			loader: "style-loader!css-loader!sass-loader"
+		},
+		]
+	},
+  devServer: { inline: true }
+};
